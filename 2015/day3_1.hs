c34fa6628b15217c2cbd6040068a37f0801f14ff
appositum <appositum@pm.me>
2023-10-03 05:26:49 -0300

    shorter day 3 pt 1 using `scanl`

--- 2015/day3_1.hs
+++ 2015/day3_1.hs
@@ -1,23 +1,13 @@
-import qualified Data.Set as S
-
-type Point = (Int, Int)
-type Set = S.Set Point
+import Data.Set (fromList)
 
 main :: IO ()
 main = readFile "inputs/day3.txt" >>= print . solve
 
 solve :: String -> Int
-solve = length . walk (0, 0) S.empty
-
-move :: Char -> Point -> Point
-move '>' (x, y) = (succ x, y)
-move '^' (x, y) = (x, succ y)
-move '<' (x, y) = (pred x, y)
-move 'v' (x, y) = (x, pred y)
-move _ p = p
-
-walk :: Point -> Set -> String -> Set
-walk _ set "" = set
-walk p set (x:xs) = walk newPoint newSet xs
-  where newPoint = move x p
-        newSet = S.insert p set
+solve = length . fromList . scanl move (0, 0)
+  where
+    move (x, y) '>' = (succ x, y)
+    move (x, y) '^' = (x, succ y)
+    move (x, y) '<' = (pred x, y)
+    move (x, y) 'v' = (x, pred y)
+    move p _ = p

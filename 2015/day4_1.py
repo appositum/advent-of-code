2f9dcca048cdbf2eef514c1333a5e7a62806f434
appositum <appositum@pm.me>
2023-10-06 00:15:03 -0300

    day 4 python
    used python because of the default hashlib module.
    it looks disgusting and takes too long to run but it worked lmao

---
+++ 2015/day4_1.py
@@ -0,0 +1,11 @@
+from hashlib import md5
+
+def solve(key: str) -> int:
+    number = 0
+
+    while md5(bytes(key + str(number), 'utf8')).hexdigest()[0:5] != '00000':
+        number += 1
+
+    return number
+
+print(solve("ckczppom"))

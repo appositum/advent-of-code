0e9f497747cffe060c1d59a334d53e66e941ee65
appositum <appositum@pm.me>
2023-10-22 22:04:20 -0300

    day 18 pt 2 rust

--- 2015/day18.rs
+++ 2015/day18.rs
@@ -1,42 +1,20 @@
 use std::fs;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Light {
-    On,
-    Off,
-}
-
-use Light::*;
-
 fn main() {
-    let matrix: Vec<Vec<char>> = vec![
-        vec!['.', '#', '.', '#', '.', '#'],
-        vec!['.', '.', '.', '#', '#', '.'],
-        vec!['#', '.', '.', '.', '.', '#'],
-        vec!['.', '.', '#', '.', '.', '.'],
-        vec!['#', '.', '#', '.', '.', '#'],
-        vec!['#', '#', '#', '#', '.', '.'],
-    ];
-
-
     let f = fs::read_to_string("inputs/day18.txt").unwrap();
-    let input: Vec<Vec<char>> = f.lines().map(|s| s.chars().collect()).collect();
+    let mut input: Vec<Vec<char>> = f.lines().map(|s| s.chars().collect()).collect();
 
-    println!("{}", solve(input, 100));
-
+    println!("part 1: {}", solve(input.clone(), 100, false));
+    let i = input.len()-1;
+    input[0][0] = '#';
+    input[0][i] = '#';
+    input[i][0] = '#';
+    input[i][i] = '#';
+    println!("part 2: {}", solve(input, 100, true));
 }
 
-fn pretty_print(matrix: &Vec<Vec<char>>) {
-    for i in matrix {
-        for j in i {
-            print!("{}", j);
-        }
-        print!("\n");
-    }
-}
-
-fn light_new_state(matrix: &Vec<Vec<char>>, coord: (usize, usize)) -> char {
-    let (mut x, mut y) = coord;
+fn light_new_state(matrix: &Vec<Vec<char>>, coord: (usize, usize), stuck_lights: bool) -> char {
+    let (x, y) = coord;
     let mut neighbors = vec![];
 
     let startx = if x > 0 { x-1 } else { x };
@@ -58,6 +36,29 @@ fn light_new_state(matrix: &Vec<Vec<char>>, coord: (usize, usize)) -> char {
         .filter(|&light| light == '#')
         .collect();
 
+    if stuck_lights {
+        match (x, y) {
+            (0, 0) => {
+                return '#';
+            },
+            (x, 0) => {
+                if x == matrix.len()-1 {
+                    return '#';
+                }
+            },
+            (0, y) => {
+                if y == matrix.len()-1 {
+                    return '#';
+                }
+            },
+            (x, y) => {
+                if x == matrix.len()-1 && y == matrix.len()-1 {
+                    return '#';
+                }
+            },
+        }
+    }
+
     if matrix[x][y] == '#' {
         if neighbors.len() == 2 || neighbors.len() == 3 {
             '#'
@@ -73,19 +74,19 @@ fn light_new_state(matrix: &Vec<Vec<char>>, coord: (usize, usize)) -> char {
     }
 }
 
-fn update_matrix(matrix: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn update_matrix(matrix: Vec<Vec<char>>, stuck_lights: bool) -> Vec<Vec<char>> {
     let mut new_matrix: Vec<Vec<char>> = matrix.clone();
 
     for i in 0..matrix.len() {
         for j in 0..matrix.len() {
-            new_matrix[i][j] = light_new_state(&matrix, (i, j));
+            new_matrix[i][j] = light_new_state(&matrix, (i, j), stuck_lights);
         }
     }
 
     new_matrix
 }
 
-fn solve(matrix: Vec<Vec<char>>, mut step: usize) -> usize {
+fn solve(matrix: Vec<Vec<char>>, mut step: usize, stuck_lights: bool) -> usize {
     let mut new_matrix = matrix.clone();
 
     loop {
@@ -93,7 +94,7 @@ fn solve(matrix: Vec<Vec<char>>, mut step: usize) -> usize {
             break;
         }
 
-        new_matrix = update_matrix(new_matrix);
+        new_matrix = update_matrix(new_matrix, stuck_lights);
         step -= 1;
     }
 
